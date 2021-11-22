2489515bdc6d91b1a9aaea2cf4f60b8fa67e7150
Isaac Novaes <isaac.novaes@hotmail.com>
2021-11-22 16:29:01 +0100

    Update file

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-"use strict"
+"use strict";
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const navLinksBG = document.querySelector("nav .nav-links-bg");
+const body = document.body;
+
+hamburger.addEventListener("click", (event) => {
+	event.currentTarget.classList.toggle("hamburger-active");
+	navLinks.classList.toggle("nav-links-active");
+	navLinksBG.classList.toggle("nav-links-bg-active");
+	body.classList.toggle("hide-scroll-bar");
+});
