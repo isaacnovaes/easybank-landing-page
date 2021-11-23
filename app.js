bb59cccf2ac4feb4aeb0ec375e51e927410ea882
Isaac Novaes <isaac.novaes@hotmail.com>
2021-11-23 13:59:44 +0100

    Add IntersectionObserver API. Add animations

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const navLinksBG = document.querySelector(".nav-links-bg");
 const body = document.body;
+const secondSectionCards = document.querySelectorAll(".second-section-container .cards-block .card");
+const thirdSectionCards = document.querySelectorAll(".third-section-container .cards-block .card");
+const footer = document.querySelector("footer");
 
 hamburger.addEventListener("click", (event) => {
 	event.currentTarget.classList.toggle("hamburger-active");
@@ -11,3 +14,33 @@ hamburger.addEventListener("click", (event) => {
 	navLinksBG.classList.toggle("nav-links-bg-active");
 	body.classList.toggle("hide-scroll-bar");
 });
+
+const options = {
+	root: null,
+	threshold: 0.5,
+};
+
+const cardObserver = new IntersectionObserver(animateCard, options);
+
+secondSectionCards.forEach((card) => cardObserver.observe(card));
+thirdSectionCards.forEach((card) => cardObserver.observe(card));
+
+function animateCard(entries, observer) {
+	const [entry] = entries;
+	if (entry.isIntersecting) {
+		entry.target.classList.add("card-observer-animate");
+		observer.unobserve(entry.target);
+	}
+}
+
+const footerObserver = new IntersectionObserver(animateFooter, options);
+
+footerObserver.observe(footer);
+
+function animateFooter(entries, observer) {
+	const [entry] = entries;
+	if (entry.isIntersecting) {
+		entry.target.classList.add("footer-observer-animate");
+		observer.unobserve(entry.target);
+	}
+}
